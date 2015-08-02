88f37da5521563281ac2c5835ddc3aab4ba9ac71
Garrett Graham <gtg240@gmail.com>
2015-08-01 21:35:11 -0400

    udpated GO automatic test and parsing functions to output more data to stdout

--- topGoAutomate.R
+++ topGoAutomate.R
@@ -1,15 +1,19 @@
-topGOAutomate<-function(nclust=14, cluster=NULL, ont=NULL, MIdata=NULL, goAnnot=NULL){
-  hclust_obj<-hclust(dist(MIdata, method = "euclidean"), method = "ward.D")
+topGOAutomate<-function(nclust=14, cluster=NULL, ont=NULL, MIdata=NULL, goAnnot=NULL, hclust_obj=NULL){
+  require(topGO)
+  if(is.null(hclust_obj)){
+    hclust_obj<-hclust(dist(MIdata, method = "euclidean"), method = "ward.D")
+  }
   k_c<-factor(as.integer(cutree(hclust_obj, k = nclust)==cluster))
   names(k_c)<-names(cutree(hclust_obj, k=14))
   #
   topGO_obj<-new("topGOdata", allGenes=k_c, annot=annFUN.gene2GO, gene2GO=goAnnot$BP, ontology="BP")
   #
-  topGO_fisher<-runTest(object = topGO_obj, algorithm = "classic", statistic = "fisher")
-  topGO_ks<-runTest(object = topGO_obj, algorithm = "classic", statistic = "ks")
-  topGO_weight<-runTest(object = topGO_obj, algorithm = "weight01", statistic = "fisher")
-  return(GenTable(topGO_obj, classic=topGO_fisher, KS=topGO_ks, weight=topGO_weight, orderBy="weight", ranksOf="classic", topNodes=20))
+  test.stat <- new("elimCount", testStatistic = GOFisherTest, name = "Elim Test", cutOff = 0.01)
+  resultElim <- getSigGroups(topGO_obj, test.stat)
+  test.stat <- new("weightCount", testStatistic = GOFisherTest, name = "Weight Test", sigRatio = "ratio")
+  resultWeight <- getSigGroups(topGO_obj, test.stat)
+  test.stat <- new("classicCount", testStatistic = GOFisherTest, name = "Classic Test")
+  resultFisher <- getSigGroups(topGO_obj, test.stat)
 
-
-  
+  return(GenTable(topGO_obj, Weight=resultWeight, Classic=resultFisher, Elim=resultElim, orderBy="Weight", ranksOf="Classic", topNodes=20))
 }
